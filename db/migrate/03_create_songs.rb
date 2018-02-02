4d1bd7d1a23665d2f01284a74dcc17f93cbc1e42
Andy Maskiell <maskiella@gmail.com>
2018-02-02 20:55:41 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,6 +1,6 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change
-    create_table :song do |t|
+    create_table :songs do |t|
       t.string :name
       t.integer :artist_id
       t.integer :genre_id
